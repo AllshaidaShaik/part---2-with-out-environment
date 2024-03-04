e0bfe3fe4551791fe9228206dd3dfe76dc218ad6
Allshida shaik <Allshaida>
2024-03-04 18:06:28 +0530

    did small changes

--- app.js
+++ app.js
@@ -20,6 +20,25 @@ connection.connect(err => {
   console.log('Connected to MySQL database');
 });
 
+const cors = require('cors');
+const corsOpts = {
+  origin: '*',
+  methods: ['GET', 'POST', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOpts));
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Access-Control-Allow-Origin,Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,Authorization'
+  );
+  next();
+});
+
 const JWT_SECRET_KEY = 'your_secret_key';
 
 app.post('/login', (req, res) => {
@@ -42,7 +61,7 @@ app.post('/login', (req, res) => {
           res.status(500).json({ error: 'Internal Server Error' });
           return;
         }
-        res.json({ login_details: user, token });
+        res.json({ data: user, token });
       });
     } else {
       res.status(401).json({ error: 'Invalid username or password' });
@@ -55,38 +74,45 @@ app.post('/form/save-details', (req, res) => {
 
   connection.query('SELECT * FROM form WHERE email = ?', [email], (error, rows) => {
     if (error) {
-      console.error('Error', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-      return;
+      console.error('Error checking email existence in database:', error);
+      return res.status(500).json({ error: 'Internal Server Error' });
     }
 
     if (rows.length > 0) {
-      res.status(400).json({ error: 'Email already exists' });
-      return;
+      return res.json({ error: 'Email already exists' });
     }
 
     connection.query('INSERT INTO form (first_name, middle_name, last_name, address, country, state, city, zip_code, email, phone_number, height, height_type, weight, active) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
       [first_name, middle_name, last_name, address, country, state, city, zip_code, email, phone_number, height, height_type, weight, 1],
       (error, results) => {
         if (error) {
-          console.error('Error', error);
-          res.status(500).json({ error: 'Internal Server Error' });
-          return;
+          console.error('Error inserting data into database:', error);
+          return res.status(500).json({ error: 'Internal Server Error' });
         }
 
         const insertedId = results.insertId;
-
         connection.query('SELECT * FROM form WHERE form_id = ?', [insertedId], (err, rows) => {
           if (err) {
-            console.error('Error', err);
-            res.status(500).json({ error: 'Internal Server Error' });
-            return;
+            console.error('Error retrieving inserted data from database:', err);
+            return res.status(500).json({ error: 'Internal Server Error' });
           }
 
           const insertedData = rows[0];
           res.json({ message: 'Form details saved successfully', data: insertedData });
         });
       });
+  });
+});
+
+
+app.get('/form/list', (req, res) => {
+  connection.query('select * from form order by form_id desc', (error, rows) => {
+    if (error) {
+      console.error('Error', error);
+      res.status(500).json({ error: 'Internal Server Error' });
+      return;
+    }
+    res.json({ data: rows });
   });
 });
 
@@ -99,7 +125,7 @@ app.delete('/form/delete-details/:id', (req, res) => {
       res.status(500).json({ error: 'Internal Server Error' });
       return;
     }
-    res.json({ message: `Form with ID ${formId} is deleted` });
+    res.json({ message: `Form with ID ${formId} is Deleted Successfully` });
   });
 });
 
